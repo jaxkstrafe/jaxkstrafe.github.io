a887498cd31d9f6927a0f6543ccb106d39ea0363
Jack Dalton <92889104+jaxkstrafe@users.noreply.github.com>
2024-05-08 13:24:43 -0500

    Changed js animation

--- MainPage/app.js
+++ MainPage/app.js
@@ -1,15 +1,15 @@
-
-const observer = new IntersectionObserver((entries) =>{
+const observer = new IntersectionObserver((entries, observer) => {
     entries.forEach((entry) => {
         console.log(entry)
-        if(entry.isIntersecting){
+        if (entry.isIntersecting) {
             entry.target.classList.add('show');
-        }
+            observer.unobserve(entry.target); 
+        } 
         else {
             entry.target.classList.remove('show');
         }
     })
-})
+});
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
